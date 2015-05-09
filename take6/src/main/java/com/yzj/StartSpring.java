fa3ec9198e8649cf52b24e89eb8d1275328e4d74
99848873 <99848873@qq.com>
2015-05-09 11:41:49 +0800

    修改启动程序方法.直接使用spring容器启动对象

--- take6/src/main/java/com/yzj/StartSpring.java
+++ take6/src/main/java/com/yzj/StartSpring.java
@@ -5,23 +5,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 载入spring的配置为文件并且启动Take6程序
+ * 
  * @author 余周锦
- * @version 2.0
- * 2015-4-12
-*/
+ * @version 2.0 2015-4-12
+ */
 public class StartSpring {
-	
-	private final static ApplicationContext config  = new ClassPathXmlApplicationContext("config/applicationContext.xml");
-		
-	public static void main(String args[]){
-				
-		config.getBean(Take6.class).start();
-		
+
+	private final static ApplicationContext config = new ClassPathXmlApplicationContext(
+			"config/applicationContext.xml");
+
+	public static void main(String args[]) {
+
 	}
-	
-	public static ApplicationContext getConfig(){
-		return config;		
+
+	public static ApplicationContext getConfig() {
+		return config;
 	}
-	
 
 }
