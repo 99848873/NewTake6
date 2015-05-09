38ea696e3951bb32a6bcc94213becc1ba417fc63
99848873 <99848873@qq.com>
2015-05-09 20:37:02 +0800

    增加判断第一次启动游戏需要启动窗口的代码

--- take6/src/main/java/com/yzj/Take6.java
+++ take6/src/main/java/com/yzj/Take6.java
@@ -1,12 +1,17 @@
 package com.yzj;
 
 import java.awt.EventQueue;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+
+import com.yzj.api.InternalWindow;
 import com.yzj.config.Preference;
 import com.yzj.util.LogConfig;
 import com.yzj.windows.MainWindow;
+import com.yzj.windows.internalwindow.InWindowCenter;
 
 /**
  * 启动主界面线程并判断初始化信息
@@ -19,6 +24,9 @@ public class Take6 {
 
 	@Resource
 	private MainWindow mainWindow;
+
+	@Resource
+	private InWindowCenter inWindowCenter;
 
 	@Resource
 	private Preference preference;
@@ -52,13 +60,16 @@ public class Take6 {
 			creatPreferenceWindow();
 		} else {
 			log.getLog().info("不是第一次启动游戏");
+			// TODO 需要删掉
+			creatPreferenceWindow();
 		}
 
 	}
 
 	/* 创建选择头像及编辑用户名的窗口 */
 	private void creatPreferenceWindow() {
-		
+		InternalWindow inWindow = inWindowCenter.getInWindow("preference1");
+		mainWindow.getBackGround().showWindow(inWindow);
 	}
 
 }
