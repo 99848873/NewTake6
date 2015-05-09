82003b7b0141fc3e18a8a54017bbc27f73bc83c1
99848873 <99848873@qq.com>
2015-05-09 20:37:50 +0800

    增加内部窗体接口

--- take6/src/main/java/com/yzj/api/InternalWindow.java
+++ take6/src/main/java/com/yzj/api/InternalWindow.java
@@ -4,11 +4,17 @@ import java.beans.PropertyVetoException;
 
 import javax.swing.JInternalFrame;
 
+/**
+ * 启动主界面线程并判断初始化信息
+ * 
+ * @author 余周锦
+ * @version 2.0 2015-5-09
+ */
 @SuppressWarnings("serial")
 public abstract class InternalWindow extends JInternalFrame {
 
 	public InternalWindow(String name) {
-		super(name);
+		super(name, false, false, false, false);
 		try {
 			setClosed(true);
 		} catch (PropertyVetoException e) {
