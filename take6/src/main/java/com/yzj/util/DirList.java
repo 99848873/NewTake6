a5dcbcfa882e86e949de00e7291b9eaaffbc9356
99848873 <99848873@qq.com>
2015-05-10 12:32:53 +0800

    格式化格式

--- take6/src/main/java/com/yzj/util/DirList.java
+++ take6/src/main/java/com/yzj/util/DirList.java
@@ -2,22 +2,19 @@ package com.yzj.util;
 
 import java.io.File;
 
-
 /**
  * 工具类，用来查看当前类路径
+ * 
  * @author 余周锦
- * @version 2.0
- * 2015-4-21
-*/
+ * @version 2.0 2015-4-21
+ */
 public class DirList {
-	
-	public static void fileList(String path){
-		
-		File file = new File(path);		
+
+	public static void fileList(String path) {
+		File file = new File(path);
 		String[] list = file.list();
-		for(String dirItem : list)
-		   System.out.println(dirItem);
-		
+		for (String dirItem : list)
+			System.out.println(dirItem);
 	}
 
 }
