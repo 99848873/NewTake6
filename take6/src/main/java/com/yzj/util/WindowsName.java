bc5df47960cd0afe9b317e7fbd973449e54058ac
99848873 <99848873@qq.com>
2015-05-10 12:34:17 +0800

    增加内部窗体枚举类新代码

---
+++ take6/src/main/java/com/yzj/util/WindowsName.java
@@ -0,0 +1,31 @@
+package com.yzj.util;
+
+/**
+ * 内部窗口名字枚举类
+ * 
+ * @author 余周锦
+ * @version 2.0 2015-5-10
+ */
+public enum WindowsName {
+
+	/**
+	 * 第一次启动游戏的偏好窗口
+	 * */
+	FirstPreference,
+
+	/**
+	 * 非第一次启动游戏的偏好窗口
+	 * */
+	Preference,
+
+	/**
+	 * "关于"菜单项的弹出窗口
+	 * */
+	About,
+
+	/**
+	 * "规则"菜单项的弹出窗口
+	 * */
+	Rule
+
+}
