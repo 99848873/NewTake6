4861d3c9469d580f00ca1f9ba51289c1e035e613
99848873 <99848873@qq.com>
2015-05-10 12:33:14 +0800

    格式化格式

--- take6/src/main/java/com/yzj/util/LogConfig.java
+++ take6/src/main/java/com/yzj/util/LogConfig.java
@@ -6,21 +6,22 @@ import org.apache.log4j.PropertyConfigurator;
 
 /**
  * 工具类,用来导入log4j的配置文件
+ * 
  * @author 余周锦
- * @version 2.0
- * 2015-4-21
-*/
+ * @version 2.0 2015-4-21
+ */
 @Component("log")
 public class LogConfig {
-	
+
 	private static final Logger logger = Logger.getLogger(LogConfig.class);
-	
-	public LogConfig(){
-		PropertyConfigurator.configure("./src/main/resources/config/log4j.properties");
+
+	public LogConfig() {
+		PropertyConfigurator
+				.configure("./src/main/resources/config/log4j.properties");
 	}
 
-	public Logger getLog(){	
-		return logger;	
+	public Logger getLog() {
+		return logger;
 	}
 
 }
